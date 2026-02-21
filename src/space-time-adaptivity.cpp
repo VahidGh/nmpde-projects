5fe16d7d6e9e8b12366f15747fea1e5ef55f38a8
Alessandro.Russi03 <aler05913@gmail.com>
2026-02-21 09:02:55 +0100

    Refactor heat problem parameters and functions

--- src/space-time-adaptivity.cpp
+++ src/space-time-adaptivity.cpp
@@ -1,6 +1,6 @@
-#include "Heat.hpp"
+#include "Parab_multi.hpp"
 
-// Main function.
+//  du/dt -div(mu * grad(u)) + div(b * u) + sigma * u = f
 int
 main(int argc, char *argv[])
 {
@@ -8,18 +8,47 @@ main(int argc, char *argv[])
 
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const auto mu = [](const Point<dim> & /*p*/) { return 0.1; };
-  const auto f  = [](const Point<dim>  &/*p*/, const double  &/*t*/) {
-    return 0.0;
+  // 1. Coefficiente di Diffusione (mu)
+  const auto mu = [](const Point<dim> & /*p*/) { 
+    return 1.0; 
   };
 
-  Heat problem(/*mesh_filename = */ "../mesh/mesh-cube-10.msh",
-               /* degree = */ 1,
+  // 2. Coefficiente di Reazione (sigma) 
+  const auto sigma = [](const Point<dim> & /*p*/) { 
+    return 0.0; 
+  };
+
+  Heat::g_function g_pulsation;
+  Heat::h_function h_spatial;
+
+  // 3. Forzante (f)
+  const auto f = [&g_pulsation, &h_spatial](const Point<dim> &p, const double &t) {
+  // Impostiamo il tempo corrente per la funzione g(t)
+  g_pulsation.set_time(t);
+  
+  // Il risultato è il prodotto g(t) * h(x) 
+  return g_pulsation.value(p) * h_spatial.value(p);
+  };
+
+  // 4. Campo di Velocità (b) 
+  const auto b = [](const Point<dim> & /*p*/) {
+    Tensor<1, dim> velocity;
+    for (unsigned int d = 0; d < dim; ++d)
+      velocity[d] = 0.0; 
+    return velocity;
+  };
+
+  // Istanziazione del problema
+  
+  Heat problem(/*mesh_file_name = */ "../mesh/mesh-cube-10.msh",
+               /* degree = */ 2,
                /* T = */ 1.0,
-               /* theta = */ 0.0,
-               /* delta_t = */ 0.0025,
-               mu,
-               f);
+               /* theta = */ 1.0,    
+               /* delta_t = */ 0.001,
+               mu,    
+               sigma, 
+               f,    
+               b);    
 
   problem.run();
 
